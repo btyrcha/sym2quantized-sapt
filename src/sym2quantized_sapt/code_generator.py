9a358ebee1a3d5600bdad0c7a8dbf5b0871c09ff
Bartosz Tyrcha <bartektyrcha123@gmail.com>
2023-02-20 11:00:42 +0100

    single tensor code_gen fix

--- src/sym2quantized_sapt/code_generator.py
+++ src/sym2quantized_sapt/code_generator.py
@@ -1,7 +1,6 @@
 import string
 from sympy import Add, Mul, Expr, expand
 from sympy.physics.secondquant import TensorSymbol
-from sym2quantized_sapt.tensors import DoubleVacuumTensorSymbol
 
 
 def _psi4numpy_indices(index: str) -> str:
@@ -70,54 +69,13 @@ def _replace_indices_names(indices: str) -> str:
     return new_indices
 
 
-def _get_einsum_for_term(term: Mul, pretty_indices=False) -> str:
+def _get_code_str(
+    coeff, cont_ind, uncont_ind, variables, pretty_indices=False
+) -> str:
 
-    if isinstance(term.args[0], TensorSymbol):
-        coeff = 1
-    else:
-        coeff = term.args[0]
-
-    indices = []
-    indices_raw = []
-    upper = []
-    lower = []
-    variables = []
-
-    for arg in term.args:
-
-        if isinstance(arg, DoubleVacuumTensorSymbol):
-
-            upper += [_psi4numpy_indices(idx.name) for idx in arg.upper()]
-            lower += [_psi4numpy_indices(idx.name) for idx in arg.lower()]
-
-            arg_indices = [
-                _psi4numpy_indices(idx.name)
-                for idx in (*arg.lower(), *arg.upper())
-            ]
-            indices.append("".join(arg_indices))
-            indices_raw += arg_indices
-
-            var_indices = [
-                _psi4numpy_indices(idx.name[0])
-                for idx in (*arg.lower(), *arg.upper())
-            ]
-            var_indices = "".join(var_indices)
-            variables.append("_".join((str(arg.symbol()), var_indices)))
-
-    # check for uncontracted indicies
-    uncont_idx = []
-    for elem in indices_raw:
-        if (elem not in lower) or (elem not in upper):
-            uncont_idx.append(elem)
-
-    # upper = set(upper)
-    # lower = set(lower)
-    # uncont_idx = (upper | lower) - (upper & lower)
-    # uncont_idx = list(uncont_idx)
-
-    indices = ",".join(indices)
-    if uncont_idx:
-        indices += "->" + "".join(uncont_idx)
+    indices = ",".join(cont_ind)
+    if uncont_ind:
+        indices += "->" + "".join(uncont_ind)
 
     if pretty_indices:
         indices = _pretty_indices_names(indices)
@@ -148,6 +106,78 @@ def _get_einsum_for_term(term: Mul, pretty_indices=False) -> str:
     return code_str
 
 
+def _get_einsum_for_Tensor(tensor: TensorSymbol, pretty_indices=True) -> str:
+
+    upper = [_psi4numpy_indices(idx.name) for idx in tensor.upper()]
+    lower = [_psi4numpy_indices(idx.name) for idx in tensor.lower()]
+
+    indices = "".join(lower + upper)
+    indices_raw = lower + upper
+
+    variable = "_".join((str(tensor.symbol()), indices))
+
+    # check for uncontracted indicies
+    uncont_ind = []
+    for elem in indices_raw:
+        if (elem not in lower) or (elem not in upper):
+            uncont_ind.append(elem)
+
+    if uncont_ind:
+        indices += "->" + "".join(uncont_ind)
+
+    if pretty_indices:
+        indices = _pretty_indices_names(indices)
+    else:
+        indices = _replace_indices_names(indices)
+
+    return '+np.einsum("{0}", {1})'.format(indices, variable)
+
+
+def _get_einsum_for_Mul(term: Mul, pretty_indices=False) -> str:
+
+    if isinstance(term.args[0], TensorSymbol):
+        coeff = 1
+    else:
+        coeff = term.args[0]
+
+    indices = []
+    indices_raw = []
+    upper = []
+    lower = []
+    variables = []
+
+    for arg in term.args:
+
+        if isinstance(arg, TensorSymbol):
+
+            upper += [_psi4numpy_indices(idx.name) for idx in arg.upper()]
+            lower += [_psi4numpy_indices(idx.name) for idx in arg.lower()]
+
+            arg_indices = [
+                _psi4numpy_indices(idx.name)
+                for idx in (*arg.lower(), *arg.upper())
+            ]
+            indices.append("".join(arg_indices))
+            indices_raw += arg_indices
+
+            var_indices = [
+                _psi4numpy_indices(idx.name[0])
+                for idx in (*arg.lower(), *arg.upper())
+            ]
+            var_indices = "".join(var_indices)
+            variables.append("_".join((str(arg.symbol()), var_indices)))
+
+    # check for uncontracted indicies
+    uncont_ind = []
+    for elem in indices_raw:
+        if (elem not in lower) or (elem not in upper):
+            uncont_ind.append(elem)
+
+    return _get_code_str(
+        coeff, indices, uncont_ind, variables, pretty_indices=pretty_indices
+    )
+
+
 def generate_einsum(expr: Expr, pretty_indices=False) -> str:
     """
     Generates string containing numpy einsum code of given expression.
@@ -161,6 +191,9 @@ def generate_einsum(expr: Expr, pretty_indices=False) -> str:
 
     expr = expand(expr)
 
+    if isinstance(expr, TensorSymbol):
+        return _get_einsum_for_Tensor(expr, pretty_indices=pretty_indices)
+
     if isinstance(expr, Add):
         return "\n".join(
             [
@@ -170,7 +203,7 @@ def generate_einsum(expr: Expr, pretty_indices=False) -> str:
         )
 
     if isinstance(expr, Mul):
-        return _get_einsum_for_term(expr, pretty_indices=pretty_indices)
+        return _get_einsum_for_Mul(expr, pretty_indices=pretty_indices)
 
-    # expr is neither Mul nor Add:
+    # expr is neither Mul, Add nor TensorSymbol:
     return ""
--- tests/test_code_generator.py
+++ tests/test_code_generator.py
@@ -3,6 +3,62 @@ from sym2quantized_sapt.tensors import DoubleVacuumTensorSymbol
 from sym2quantized_sapt.code_generator import generate_einsum
 
 
+def test_single_self_contraction():
+
+    reference = """+np.einsum("kk", A_kk)"""
+
+    k = symbols("k", below_fermi=True)
+    A = DoubleVacuumTensorSymbol("A", (k,), (k,))
+
+    tested_expr = generate_einsum(A)
+
+    assert reference == tested_expr
+
+
+def test_doucle_self_contraction():
+
+    reference = """+np.einsum("klkl", A_klkl)"""
+
+    k, l = symbols("k l", below_fermi=True)
+    A = DoubleVacuumTensorSymbol(
+        "A",
+        (
+            k,
+            l,
+        ),
+        (
+            k,
+            l,
+        ),
+    )
+
+    tested_expr = generate_einsum(A)
+
+    assert reference == tested_expr
+
+
+def test_partial_self_contraction():
+
+    reference = """+np.einsum("kmkl->ml", A_kmkl)"""
+
+    k, l, m = symbols("k l m", below_fermi=True)
+    A = DoubleVacuumTensorSymbol(
+        "A",
+        (
+            k,
+            l,
+        ),
+        (
+            k,
+            m,
+        ),
+    )
+
+    tested_expr = generate_einsum(A)
+
+    assert reference == tested_expr
+
+
 def test_two_eris():
 
     reference = """-np.einsum("abrs,rsab", v_abrs, v_rsab)"""
@@ -13,8 +69,28 @@ def test_two_eris():
     b = symbols("b", is_molB=True, above_fermi=True)
     j = symbols("j", is_molB=True, below_fermi=True)
 
-    v_ijab = DoubleVacuumTensorSymbol("v", (a, b,), (i, j,))
-    v_abij = DoubleVacuumTensorSymbol("v", (i, j,), (a, b,))
+    v_ijab = DoubleVacuumTensorSymbol(
+        "v",
+        (
+            a,
+            b,
+        ),
+        (
+            i,
+            j,
+        ),
+    )
+    v_abij = DoubleVacuumTensorSymbol(
+        "v",
+        (
+            i,
+            j,
+        ),
+        (
+            a,
+            b,
+        ),
+    )
 
     tested_expr = generate_einsum((-1.0) * v_ijab * v_abij)
 
