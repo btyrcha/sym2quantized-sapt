80c7956fe35efc3230029bccc80635029e1f3012
Filip Brzęk <filip.brzek@gmail.com>
2023-12-10 19:26:12 +0100

    ci: make linting script run against master diff by a default

--- .gitlab/ci/utils/lint_python_changes.sh
+++ .gitlab/ci/utils/lint_python_changes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # set the default values for local scripts debugging
-CI_MERGE_REQUEST_TARGET_BRANCH_NAME=${CI_MERGE_REQUEST_TARGET_BRANCH_NAME:-developer}
+CI_MERGE_REQUEST_TARGET_BRANCH_NAME=${CI_MERGE_REQUEST_TARGET_BRANCH_NAME:-master}
 
 # names only
 # filter out deleted files
@@ -13,7 +13,7 @@ CHANGED_FILES=$(git diff --diff-filter=d --name-only "origin/${CI_MERGE_REQUEST_
 #EXCEPTIONS=""
 #CHANGED_FILES=$(echo ${CHANGED_FILES//$EXCEPTIONS/} | tr -d " ")
 
-if [ ! -z "$CHANGED_FILES" ];then
+if [ ! -z "$CHANGED_FILES" ]; then
   echo "Running pylint on ${CHANGED_FILES}"
   python3 -m pylint --rcfile=.pylintrc --output-format=text $CHANGED_FILES
   exit $?
